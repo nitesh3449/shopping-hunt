b6134eb7dc5003bb5bb84b8030274bb4c28055a0
nitesh3449 <niteshkumarverma4@gmail.com>
2020-01-30 14:29:04 +0530

    Password links

---
+++ models/passwordlink.js
@@ -0,0 +1,6 @@
+//https://stackoverflow.com/questions/6951563/storing-passwords-with-node-js-and-mongodb
+//https://stackoverflow.com/questions/43092071/how-should-i-store-salts-and-passwords-in-mongodb
+//https://stackoverflow.com/questions/14588032/mongoose-password-hashing
+//http://devsmash.com/blog/password-authentication-with-mongoose-and-bcrypt
+//https://stackoverflow.com/questions/51818474/keep-hashed-password-on-update-using-nodejs-mongo-and-express
+//https://stackoverflow.com/questions/39893815/mongodb-how-to-store-credentials-safely
