69807cd0ea2b25f73ef614ff50eeecaf5b3db628
niteshtatras <niteshkumarverma4@gmail.com>
2020-08-21 12:22:25 +0530

    change

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const checkToken = require('../auth/auth'); 
 const routes = express.Router();
-const RegistrationController = require('../controller/RegistrationController');
-const ProductController = require('../controller/ProductController');
 const HomeController = require('../controller/HomeController');
+// const RegistrationController = require('../controller/RegistrationController');
+const ProductController = require('../controller/ProductController');
 
-routes.post('/registerMe',  RegistrationController.regMe); 
-routes.post('/login',  RegistrationController.login); 
-routes.get('/getProfile', checkToken, RegistrationController.getProfile); 
-routes.post('/updateUserProfile', checkToken, RegistrationController.updateUserProfile);
-routes.post('/updateUserImage', checkToken, RegistrationController.updateUserImage);
+// routes.post('/registerMe',  RegistrationController.regMe); 
+// routes.post('/login',  RegistrationController.login); 
+// routes.get('/getProfile', checkToken, RegistrationController.getProfile); 
+// routes.post('/updateUserProfile', checkToken, RegistrationController.updateUserProfile);
+// routes.post('/updateUserImage', checkToken, RegistrationController.updateUserImage);
 
 routes.post('/fetchProducts', checkToken, ProductController.fetchProductList);
 routes.post('/addNewProduct', checkToken, ProductController.addNewProduct);
